22ca837519272080ced431193b88201437a445fa
JATIN KATIYAR <katiyarjatin943@gmail.com>
2024-12-07 13:32:39 +0530

    Update Fibonacci.v

--- Fibonacci.v
+++ Fibonacci.v
@@ -2,26 +2,22 @@
 //name-jatin katiyar
 module vector_array_example(
 );
-     reg [3:0]out[7:0] ;
          reg [3:0]count[7:0] ;
      integer i; 
   initial 
   begin 
   count[0]=0;
-  out[0]=0;
   count[1]=1;
-  out[1]=1;
   end  
   
   initial 
   for(i=2; i< 8; i = i+1)begin
   count[i]= count[i-1]+ count[i-2];
-  out[i]=count[i];
   end
   
   initial                
   for(i=0; i< 8; i = i+1)
-  $display("output=%d",out[i]);
+       $display("output=%d",count[i]);
   
 endmodule
 //TCL console
